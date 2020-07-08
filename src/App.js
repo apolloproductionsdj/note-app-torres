fb145cd40ff470def345b46a3166a90d901c04e6
Mike Torres <apolloproductionsdj@gmail.com>
2020-07-08 21:25:15 +0000

    added List component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <List />
+      </div>
+    );
+  }
 }
 
 export default App;
