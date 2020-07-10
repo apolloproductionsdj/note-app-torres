2c13632c242f1495935f6fd4f52569e24f7b8491
Mike Torres <apolloproductionsdj@gmail.com>
2020-07-10 18:20:13 +0000

    add onClick handler to tag

--- src/components/Note.js
+++ src/components/Note.js
@@ -12,14 +12,21 @@ class Note extends React.Component {
     this.props.submitNote(formData, this.props.note.id);
   }
 
-renderTagForm() {
-  return (
-    <span>
-      Tag your note:
-      <i className="tag-button material-icons">add circle</i>
-    </span>
-  );
-};
+  renderTagForm() {
+    if (!this.props.newTag) {
+      return (
+        <span>
+          Tag your note:
+          <i 
+            className="tag-button material-icons"
+            onClick={() => this.props.showTagForm()}
+          >
+            add circle
+          </i>
+        </span>
+      );
+    }
+  };
 
   render() {
     const { note } = this.props;
